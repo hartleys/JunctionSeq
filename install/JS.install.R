6f07e9615ba372ec0a0c9966f926f1ae1668cdb0
hartleys <stephen.hartley.phd@gmail.com>
2015-11-11 16:19:40 -0500

    added install script

---
+++ install/JS.install.R
@@ -0,0 +1,55 @@
+
+
+JS.install <- function(...){
+  message("Installing dependencies...");
+
+  message("Installing CRAN package dependencies... ",date());
+  message("   This can be called directly using the function JS.install.CRAN.dependencies(...)");
+  JS.install.CRAN.dependencies(...);
+  message("Done installing CRAN package dependencies. ",date())));
+
+  message("Installing Bioconductor package dependencies... ",date());
+  message("   This can be called directly using the function JS.install.BIOC.dependencies(...)");
+  JS.install.BIOC.dependencies(...);
+  message("Done installing Bioconductor package dependencies. ",date());
+
+  message("Done installing package dependencies. ",date());
+
+  message("Beginning JunctionSeq Installation... ",date());
+  if(.Platform$OS.type == "unix"){
+    message("Auto-detected unix-like system. Attempting to install from source...");
+    install.packages("http://hartleys.github.io/JunctionSeq/install/JunctionSeq_LATEST.tar.gz",repos=NULL, type="source");
+  } else {
+    message("Auto-detected windows system. Attempting to install from source using Rtools...");
+    install.packages("http://hartleys.github.io/JunctionSeq/install/JunctionSeq_LATEST.tar.gz",repos=NULL, type="source");
+  }
+  message("Installation complete. ",date());
+
+  message("Testing to see if package can be loaded... ",date());
+  library("JunctionSeq");
+  message("Done.",date());
+}
+
+JS.install.CRAN.dependencies <- function(...){
+  if(! suppressWarnings(suppressPackageStartupMessages(require("statmod")))) install.packages("statmod", ...)
+  if(! suppressWarnings(suppressPackageStartupMessages(require("plotrix")))) install.packages("plotrix", ...)
+  if(! suppressWarnings(suppressPackageStartupMessages(require("stringr")))) install.packages("stringr", ...)
+  if(! suppressWarnings(suppressPackageStartupMessages(require("Rcpp")))) install.packages("Rcpp", ...)
+  if(! suppressWarnings(suppressPackageStartupMessages(require("RcppArmadillo")))) install.packages("RcppArmadillo", ...)
+  if(! suppressWarnings(suppressPackageStartupMessages(require("ggplot2")))) install.packages("ggplot2", ...)
+  if(! suppressWarnings(suppressPackageStartupMessages(require("locfit")))) install.packages("locfit", ...)
+  if(! suppressWarnings(suppressPackageStartupMessages(require("Hmisc")))) install.packages("Hmisc", ...)
+}
+
+JS.install.BIOC.dependencies <- function(...){
+  source("http://bioconductor.org/biocLite.R");
+  biocLite();
+  if(! suppressWarnings(suppressPackageStartupMessages(require("Biobase")))) biocLite("Biobase", ...);
+  if(! suppressWarnings(suppressPackageStartupMessages(require("BiocGenerics")))) biocLite("BiocGenerics", ...);
+  if(! suppressWarnings(suppressPackageStartupMessages(require("BiocParallel")))) biocLite("BiocParallel", ...);
+  if(! suppressWarnings(suppressPackageStartupMessages(require("GenomicRanges")))) biocLite("GenomicRanges", ...);
+  if(! suppressWarnings(suppressPackageStartupMessages(require("IRanges")))) biocLite("IRanges", ...);
+  if(! suppressWarnings(suppressPackageStartupMessages(require("S4Vectors")))) biocLite("S4Vectors", ...);
+  if(! suppressWarnings(suppressPackageStartupMessages(require("genefilter")))) biocLite("genefilter", ...);
+  if(! suppressWarnings(suppressPackageStartupMessages(require("geneplotter")))) biocLite("geneplotter", ...);
+}
